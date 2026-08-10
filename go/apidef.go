239897fb0130159ce860e19120ab623d559944fd
Richard Rodger <richard@ricebridge.com>
2026-08-10 10:17:32 +0000

    go: v0.1.4

--- go/apidef.go
+++ go/apidef.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const Version = "0.1.2"
+const Version = "0.1.4"
 
 // ApiDef creates a new API definition generator with the given options.
 func NewApiDef(opts ApiDefOptions) *apiDefInstance {
